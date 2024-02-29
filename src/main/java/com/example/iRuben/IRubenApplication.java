aa56caebe5a1ae9a2af5b098acbeefe467a6314f
az3l1t <eternity_cr9p@mail.ru>
2024-02-29 10:56:01 +0300

    testOfCommit

---
+++ src/main/java/com/example/iRuben/IRubenApplication.java
@@ -0,0 +1,13 @@
+package com.example.iRuben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IRubenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IRubenApplication.class, args);
+	}
+
+}
